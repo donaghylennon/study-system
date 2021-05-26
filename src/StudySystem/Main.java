5ad07117a5ebc0090e0ae6215d9b3c46e4fdc330
Lennon Donaghy <donaghylennon@gmail.com>
2021-05-26 19:35:22 +0100

    Worked out general layout of main screen

--- src/StudySystem/Main.java
+++ src/StudySystem/Main.java
@@ -3,6 +3,22 @@ package StudySystem;
 public class Main {
 
 	public static void main(String[] args) {
+//		try {
+//			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel"); //UIManager.getSystemLookAndFeelClassName());  // com.sun.java.swing.plaf.gtk.GTKLookAndFeel
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (InstantiationException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IllegalAccessException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (UnsupportedLookAndFeelException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		JFrame.setDefaultLookAndFeelDecorated(true);
 		
 		StudySystemFrame frame = new StudySystemFrame();
 		
--- src/StudySystem/StudySystemFrame.java
+++ src/StudySystem/StudySystemFrame.java
@@ -1,28 +1,79 @@
 package StudySystem;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class StudySystemFrame extends JFrame {
 	
 	public StudySystemFrame() {
+		
+		JPanel leftPanel = new JPanel();
+		
+		JPanel generalPanel = new JPanel();
+		generalPanel.setBackground(new Color(40, 80, 220));
+//		generalPanel.setBounds(0, 60, 400, 150);
+		generalPanel.setPreferredSize(new Dimension(400, 150));
+//		generalPanel.setAlignmentX(LEFT_ALIGNMENT);
+//		generalPanel.setAlignmentY(TOP_ALIGNMENT);
+		JPanel navPanel = new JPanel();
+		navPanel.setBackground(Color.gray);
+//		navPanel.setBounds(0, 0, 800, 60);
+		navPanel.setPreferredSize(new Dimension(800, 60));
+//		navPanel.setAlignmentY(TOP_ALIGNMENT);
+		JPanel coursePanel = new JPanel();
+		coursePanel.setBackground(new Color(40, 80, 180));
+//		coursePanel.setBounds(0, 210, 400, 390);
+		coursePanel.setPreferredSize(new Dimension(400, 390));
+//		coursePanel.setAlignmentX(LEFT_ALIGNMENT);
+//		coursePanel.setAlignmentY(BOTTOM_ALIGNMENT);
+		JPanel calendarPanel = new JPanel();
+		calendarPanel.setBackground(new Color(70, 80, 100));
+//		calendarPanel.setBounds(400, 60, 400, 540);
+		calendarPanel.setPreferredSize(new Dimension(400, 540));
+		
+//		JButton button = new JButton();
+//		button.setBounds(10, 10, 100, 50);
+//		button.setBackground(new Color(200, 200, 200));
+//		button.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+//		button.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+		
+		JLabel generalLabel = new JLabel();
+		generalLabel.setText("General Information");
+		generalPanel.add(generalLabel);
+		
 		this.setTitle("Study System");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
-		this.setSize(600,400);
+		this.setLayout(new BorderLayout());
+		leftPanel.setLayout(new BorderLayout());
+//		this.setResizable(false);
+		this.setSize(800,600);
+		this.setLocationRelativeTo(null);
 		
 		ImageIcon logo = new ImageIcon("res/StudySystem2.png");
 		this.setIconImage(logo.getImage());
 		
+		this.add(leftPanel, BorderLayout.WEST);
+
+		leftPanel.add(generalPanel, BorderLayout.NORTH);
+
+		this.add(navPanel, BorderLayout.NORTH);
+
+		leftPanel.add(coursePanel, BorderLayout.CENTER);
+
+		this.add(calendarPanel, BorderLayout.CENTER);
+		
+//		this.add(button);
+
+//		SwingUtilities.updateComponentTreeUI(this);
+		
 		this.setVisible(true);
 		
-		this.getContentPane().setBackground(new Color(64,0,128));
+//		this.getContentPane().setBackground(new Color(64,0,128));
 	}
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
 }
